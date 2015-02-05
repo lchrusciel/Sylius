11afaf86bc18be217f614bbb9407b9df3edf1910
Mateusz Zalewski <zaleslaw@gmail.com>
2015-02-05 12:22:39 +0100

    Finished CsvWriter implementation. Test command.

--- src/Sylius/Component/ImportExport/Writer/CsvWriter.php
+++ src/Sylius/Component/ImportExport/Writer/CsvWriter.php
@@ -12,15 +12,32 @@
 namespace Sylius\Component\ImportExport\Writer;
 
 use Doctrine\ORM\EntityManager;
+use EasyCSV\Writer;
 
 /**
  * @author Mateusz Zalewski <mateusz.zalewski@lakion.com>
  */
 class CsvWriter implements WriterInterface
 {
+    /**
+     * Is EasySCV\Writer initialized
+     *
+     * @var boolean
+     */
+    private $running = false;
+
+    /**
+     * @var Writer
+     */
+    private $csvWriter;
+
     public function write(array $items, array $configuration)
     {
-        
+        if (!$this->running) {
+            $this->csvWriter = new Writer($configuration["file"], 'w');
+        }
+
+        $this->csvWriter->writeFromArray($items);
     }
 
     /**
