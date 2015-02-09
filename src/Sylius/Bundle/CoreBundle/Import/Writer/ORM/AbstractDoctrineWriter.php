8f119aac30b53faac690c50fd37b2c0f3f68ee19
Lukasz Adrian Chrusciel <lchrusciel@gmail.com>
2015-02-09 16:03:20 +0100

    [WIP] ProductWriter

--- src/Sylius/Bundle/CoreBundle/Import/Writer/ORM/AbstractDoctrineWriter.php
+++ src/Sylius/Bundle/CoreBundle/Import/Writer/ORM/AbstractDoctrineWriter.php
@@ -24,10 +24,10 @@ abstract class AbstractDoctrineWriter implements WriterInterface
     
     public function write(array $items)
     {   
-        foreach ($items as $item)
-        {            
-            $item = $this->process($item);
-        }
+        // foreach ($items as $item)
+        // {            
+            $item = $this->process($items);
+        // }
         
         $em = $this->getDoctrine()->getManager();
 
